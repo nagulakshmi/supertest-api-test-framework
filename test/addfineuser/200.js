61674e5a7fd2ccb464e38e14b2604fc4a81fdbfb
nagalakshmi <lakshmi.padumanabha@gmail.com>
2019-12-12 22:41:02 +0530

    fixed checkvaliduser

--- test/addfineuser/200.js
+++ test/addfineuser/200.js
@@ -1,6 +1,6 @@
 
 describe('POST/api/addfineuser', function () {
-  it.only('it should return data sucessfully inserted', function () {
+  it('it should return data sucessfully inserted', function () {
     return httprequest.httpPostRequest('/api/addfineuser')
       .send({ "bill_no": "null", "name": "hari", "mobile_no": "9788207185", "division": 1, "ward_no": 4, "door_no": 1, "street_name": "KIRAPPATTI", "fine_dep": 1, "fine_for": 4, "fine_amt": 3000, "mode_of_pay": "Cash", "lat": "0", "lang": "0", "fine_user_id": 11357, "fine_user_name": "Paneerselvam", "fine_user_contact": "8072561814", "fineimg1": "null", "fineimg2": "null" })
       .expect(200)
