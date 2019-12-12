2468b0a6a3d7535fbe98ba0fb15335379f54f110
nagalakshmi <lakshmi.padumanabha@gmail.com>
2019-12-12 23:11:55 +0530

    FIN-2:fixed for editprofile database value check

--- test/editprofile/200.js
+++ test/editprofile/200.js
@@ -1,13 +1,18 @@
 
 describe('post /api/editprofile',function(){
-    it('it should return Profile Updated Successfully',function(){
+    it.only('it should return Profile Updated Successfully',function(){
       return httprequest.httpPostRequest('/api/editprofile')
-      .send({"id":"11372","address":"ramijnagar","wardno":"59"})
+      .send({"id":"11372","phone":"8056665041","division":"6","address":"RMS colony","wardno":"20"})
       .expect(200)
       .then(response =>{
-        console.log(JSON.stringify(response.body));
-        assert.deepEqual(response.body,{"success":"true","result":"Profile Updated Successfully"})
+         assert.deepEqual(response.body,{"success":"true","result":"Profile Updated Successfully"})
+        return dbConnection.excecuteQuery("select * from users where id = 11372")
+        .then(result => {
+          assert.equal(result.length, 1);
+          assert.deepEqualExcluding(result[0],{"phone":"8056665041","division":"6","address": "RMS colony","wardno":"20"},["id","role","username","email","password","otp_number","remember_token","created_at","updated_at","activate","access","department","mid"])
+        })
       })
     })
   });
 
+  
